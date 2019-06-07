8a6bf5d9771d835a2529b1c5bc6625ec03415894
aman tiwari <amantiwari12081999@gmail.com>
2019-06-07 19:50:54 +0530

    added instagram and facebook oauth

--- index.js
+++ index.js
@@ -11,53 +11,53 @@ app.get('/', (req,res) => {
   res.send({ name: 'aman tiwari'})
 });
 
-//
-// //GOOGLE OAUTH
-// passport.use(new GoogleStrategy({
-//   clientID: auth.googleClientID,
-//   clientSecret: auth.googleClientSecret,
-//   callbackURL: '/auth/google/callback'
-// },
-//  (accessToken, refreshToken, profile, done) => {
-//    console.log(accessToken)
-//    console.log(refreshToken)
-//    console.log(profile)
-//  }
-// ))
-//
-// app.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile']}));
-//
-// app.get('/auth/google/callback', passport.authenticate('google'))
-//
-//
-// //FACEBOOK OAUTH
-// passport.use( new FacebookStrategy({
-//   clientID: auth.facebookClientID,
-//   clientSecret: auth.facebookClientSecret,
-//   callbackURL: '/auth/facebook/callback'
-// },
-//  (accessToken,refreshToken, profile, done) => {
-//    console.log(accessToken)
-//    console.log(profile)
-//  }
-// ))
-//
-// app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email','user_location','user_photos']}));
-// app.get('/auth/facebook/callback', passport.authenticate('facebook'));
-//
-// //INSTAGRAM oauth
-//
-// passport.use( new InstagramStrategy({
-//   clientID: auth.instagramClientID,
-//   clientSecret: auth.instagramClientSecret,
-//   callbackURL: '/auth/instagram/callback'
-// },
-// (accessToken, refreshToken, profile, done) => {
-//   console.log(accessToken)
-// }))
-//
-// app.get('/auth/instagram', passport.authenticate('instagram', { scope: ['comments', 'relationships']}));
-// app.get('auth/instagram/callback', passport.authenticate('instagram'))
+
+//GOOGLE OAUTH
+passport.use(new GoogleStrategy({
+  clientID: auth.googleClientID,
+  clientSecret: auth.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+},
+ (accessToken, refreshToken, profile, done) => {
+   console.log(accessToken)
+   console.log(refreshToken)
+   console.log(profile)
+ }
+))
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile']}));
+
+app.get('/auth/google/callback', passport.authenticate('google'))
+
+
+//FACEBOOK OAUTH
+passport.use( new FacebookStrategy({
+  clientID: auth.facebookClientID,
+  clientSecret: auth.facebookClientSecret,
+  callbackURL: '/auth/facebook/callback'
+},
+ (accessToken,refreshToken, profile, done) => {
+   console.log(accessToken)
+   console.log(profile)
+ }
+))
+
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email','user_location','user_photos']}));
+app.get('/auth/facebook/callback', passport.authenticate('facebook'));
+
+//INSTAGRAM oauth
+
+passport.use( new InstagramStrategy({
+  clientID: auth.instagramClientID,
+  clientSecret: auth.instagramClientSecret,
+  callbackURL: '/auth/instagram/callback'
+},
+(accessToken, refreshToken, profile, done) => {
+  console.log(accessToken)
+}))
+
+app.get('/auth/instagram', passport.authenticate('instagram', { scope: ['comments', 'relationships']}));
+app.get('auth/instagram/callback', passport.authenticate('instagram'))
 
 
 const port = process.env.PORT || 3000
